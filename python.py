b7dc05834babc2489c5310bb1d3791cc918abbf5
JustMurple <lomakajordan191@gmail.com>
2026-05-26 19:24:55 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,76 @@
+def round(p):
+    if p==1:
+        p=0
+    elif p==0: p=1
+    return p
+
+def move(p):
+    i,j=0,0
+    print(p, "move")
+    m=int(input("Where do you want to move: "))
+    if m<0 or 9<m:
+        print("Invalid Move")
+        return [5,5]
+    
+    if m<=3:
+        i=0
+    elif m<=6:
+        i=1
+    else: i=2
+    
+    if m%3==1: 
+        j=0
+    elif m%3==2:
+        j=1
+    else: j=2
+    return [i,j]
+
+def show(B):
+    print(B[0][0], "|", B[0][1], "|", B[0][2])
+    print(B[1][0], "|", B[1][1], "|", B[1][2])
+    print(B[2][0], "|", B[2][1], "|", B[2][2])
+
+def regMove(p, B):
+    val=move(p)
+    x = True
+    if val==[5,5] or B[val[0]][val[1]]!="-":
+        x=False
+    while(x==False):
+        val=move(p)
+        if val!=[5,5] and B[val[0]][val[1]]=="-":
+            x=True
+    if(p==1):
+        B[val[0]][val[1]]="X"
+    else: B[val[0]][val[1]]="O"
+
+def checkWin(B):
+    if(B[0][0]==B[0][1]==B[0][2]!="-"):
+        return True
+    elif(B[1][0]==B[1][1]==B[1][2]!="-"):
+        return True
+    elif(B[2][0]==B[2][1]==B[2][2]!="-"):
+        return True
+    elif(B[0][0]==B[1][0]==B[2][0]!="-"):
+        return True
+    elif(B[0][1]==B[1][1]==B[2][1]!="-"):
+        return True
+    elif(B[0][2]==B[1][1]==B[2][2]!="-"):
+        return True
+    elif(B[0][0]==B[1][1]==B[2][2]!="-"):
+        return True
+    elif(B[0][2]==B[1][1]==B[2][0]!="-"):
+        return True
+    else: return False
+
+p=1
+
+B = [["-","-","-"],
+     ["-","-","-"],
+     ["-","-","-"]]
+
+while(checkWin(B)==False):
+    show(B)
+    regMove(p, B)
+    p=round(p)
+
+print(round(p), "Won!")
