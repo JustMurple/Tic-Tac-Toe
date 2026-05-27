7042834071a480e14de7847865042390b93e489d
JustMurple <lomakajordan191@gmail.com>
2026-05-27 18:15:51 +0200

    final touches

--- python.py
+++ python.py
@@ -1,16 +1,19 @@
-def round(p):
+def switch_player(p):
     if p==1:
         p=0
-    elif p==0: p=1
+    else: p=1
     return p
 
 def move(p):
     i,j=0,0
     print(p, "move")
-    m=int(input("Where do you want to move: "))
-    if m<0 or 9<m:
+    try: 
+        m=int(input("Where do you want to move: "))
+    except ValueError:
+        print("Please enter an integer 1-9")
+    if m<1 or 9<m:
         print("Invalid Move")
-        return [5,5]
+        return None
     
     if m<=3:
         i=0
@@ -30,41 +33,42 @@ def show(B):
     print(B[1][0], "|", B[1][1], "|", B[1][2])
     print(B[2][0], "|", B[2][1], "|", B[2][2])
 
-def regMove(p, B):
+def make_Move(p, B):
     val=move(p)
     x = True
-    if val==[5,5] or B[val[0]][val[1]]!="-":
+    if val==None or B[val[0]][val[1]]!="-":
         x=False
-    while(x==False):
+    while x==False:
         val=move(p)
-        if val!=[5,5] and B[val[0]][val[1]]=="-":
+        if val!=None and B[val[0]][val[1]]=="-":
             x=True
-    if(p==1):
+    if p==1:
         B[val[0]][val[1]]="X"
     else: B[val[0]][val[1]]="O"
 
-def checkWin(B):
-    if(B[0][0]==B[0][1]==B[0][2]!="-"):
+def check_Win(B):
+    if B[0][0]==B[0][1]==B[0][2]!="-":
         return True
-    elif(B[1][0]==B[1][1]==B[1][2]!="-"):
+    elif B[1][0]==B[1][1]==B[1][2]!="-":
         return True
-    elif(B[2][0]==B[2][1]==B[2][2]!="-"):
+    elif B[2][0]==B[2][1]==B[2][2]!="-":
         return True
-    elif(B[0][0]==B[1][0]==B[2][0]!="-"):
+    elif B[0][0]==B[1][0]==B[2][0]!="-":
         return True
-    elif(B[0][1]==B[1][1]==B[2][1]!="-"):
+    elif B[0][1]==B[1][1]==B[2][1]!="-":
         return True
-    elif(B[0][2]==B[1][2]==B[2][2]!="-"):
+    elif B[0][2]==B[1][2]==B[2][2]!="-":
         return True
-    elif(B[0][0]==B[1][1]==B[2][2]!="-"):
+    elif B[0][0]==B[1][1]==B[2][2]!="-":
         return True
-    elif(B[0][2]==B[1][1]==B[2][0]!="-"):
+    elif B[0][2]==B[1][1]==B[2][0]!="-":
         return True
     else: return False
-def checkTie(B):
+
+def check_Tie(B):
     for r in B:
         for c in r:
-            if(c=="-"): return False
+            if c=="-": return False
     return True
 p=1
 
@@ -72,11 +76,13 @@ B = [["-","-","-"],
      ["-","-","-"],
      ["-","-","-"]]
 
-while(checkWin(B)==False and checkTie(B)==False):
-    show(B)
-    regMove(p, B)
-    p=round(p)
+show(B)
 
-if(checkWin(B)==True):
-    print(round(p), "Won!")
+while not check_Win(B) and not check_Tie(B):
+    make_Move(p, B)
+    show(B)
+    p=switch_player(p)
+
+if check_Win(B):
+    print(switch_player(p), "Won!")
 else: print("It's a tie :(")
